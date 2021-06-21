aa679e7f92dc92af3005df7780fa089a5e2a9745
masi <masiunnisa.13@gmail.com>
2021-06-21 19:04:24 +0530

    Initial commit

---
+++ question_model.py
@@ -0,0 +1,7 @@
+# create a question class with 2 attributes: text and answer
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
